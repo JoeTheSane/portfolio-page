61bd115cf2647f9ea59061eecbf79c8b85cd0f4b
joethesane <joethesane@proton.me>
2023-08-06 12:42:34 -0400

    Day 22 - render application titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 
 st.set_page_config(layout = "wide")
@@ -19,3 +20,17 @@ Below, you can find some of the apps I have built in Python. Feel free to contac
 """
 
 st.write(content2)
+
+csv_dataframe = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+
+with col3:
+    for index, row in csv_dataframe[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in csv_dataframe[10:].iterrows():
+        st.header(row["title"])
