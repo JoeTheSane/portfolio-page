b74d3ee0f0d08f4d4f653ac8ac520ed03c9464e9
joethesane <joethesane@proton.me>
2023-08-07 07:47:47 -0400

    Day 23 - Send email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 
 st.header("Contact Me")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        
